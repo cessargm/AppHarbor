b429185a96909a479d50b4afd1bb9b16e86d6d23
montero.cesargonzalo@gmail.com <montero.cesargonzalo@gmail.com>
2016-03-29 12:13:46 -0600

    ObtenerUsuariosCercanos

--- Closer.Services/Data/Datos.cs
+++ Closer.Services/Data/Datos.cs
@@ -14,7 +14,14 @@ namespace Closer.Services.Data
         public const double _maximoLatitud = 10;
         static public List<InformacionBasica> ObtenerUsuariosCercanos(this Ubicacion ubicacion)
         {
-            return usuarios.Where(u => (u.Ubicacion.Latitud <= ubicacion.Latitud - _maximoLatitud || u.Ubicacion.Latitud >= ubicacion.Latitud + _maximoLatitud) && (u.Ubicacion.Longitud <= ubicacion.Longitud - _maximoLongitud || u.Ubicacion.Longitud >= ubicacion.Longitud + _maximoLongitud)).Select(u => u.MiInformacion).ToList();
+            var longitudMax = ubicacion.Longitud + _maximoLongitud;
+            var longitudMin = ubicacion.Longitud - _maximoLongitud;
+            var latitudMax = ubicacion.Latitud + _maximoLatitud;
+            var latitudMin = ubicacion.Latitud - _maximoLatitud;
+
+            return usuarios.Where(u => (u.Ubicacion.Longitud >= longitudMin || u.Ubicacion.Longitud <= longitudMax) && (u.Ubicacion.Latitud >= latitudMin || u.Ubicacion.Latitud  <= latitudMax)).Select(u => u.MiInformacion).ToList();
+
+            //return usuarios.Where(u => (u.Ubicacion.Latitud <= ubicacion.Latitud - _maximoLatitud || u.Ubicacion.Latitud >= ubicacion.Latitud + _maximoLatitud) && (u.Ubicacion.Longitud <= ubicacion.Longitud - _maximoLongitud || u.Ubicacion.Longitud >= ubicacion.Longitud + _maximoLongitud)).Select(u => u.MiInformacion).ToList();
         }
     }
 
